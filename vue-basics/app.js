2f86bf198d46a20256b5a87efb9c9527ce084074
Julie Sultanova <iuliiasult@gmail.com>
2025-10-28 15:06:50 +0900

    toggle fav challenge

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -3,15 +3,20 @@ const { createApp, ref } = Vue;
 const app = createApp({
   data() {
     return {
-      url: "https://example.com",
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
         this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
     }
   }
 });
